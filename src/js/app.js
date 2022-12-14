82087c664fb3a56792a84ff1b3f4331340f1c198
Maria <nikmaria15@gmail.com>
2022-12-14 13:23:39 +0500

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,18 @@
 export default function healthStatus(data) {
   let health;
-  if (data.name !== undefined || data.health !== undefined) {
-    switch (true) {
-      case data.health > 50:
-        health = 'healthy';
-        break;
-      case data.health <= 50 && data.health >= 15:
-        health = 'wounded';
-        break;
-      case data.health < 15 && data.health > 0:
-        health = 'critical';
-        break;
-      default:
-        health = 'dead';
-    }
+  if (data.name === undefined || data.health === undefined || typeof data.health === 'string') {
+    return 0;
   }
+
+  if (data.health > 50) {
+    health = 'healthy';
+  } else if (data.health <= 50 && data.health >= 15) {
+    health = 'wounded';
+  } else if (data.health < 15 && data.health > 0) {
+    health = 'critical';
+  } else {
+    health = 'dead';
+  }
+
   return health;
 }
